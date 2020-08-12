a57b9a43dd3592ca21e8762ab8e725d7fc87852d
Robert Meurer <robertmeureraraujo@gmail.com>
2020-08-12 19:49:25 -0300

    production config

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require("express");
+require("dotenv").config();
 
+const express = require("express");
+const cors = require("cors");
 const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // ------------------------------------------------------------
@@ -12,9 +15,8 @@ const scraps = [];
 
 // ------------------------------------------------------------
 
-function logRequests(request, response, next){
-
-  const {method, url} = request;
+function logRequests(request, response, next) {
+  const { method, url } = request;
   const logLabel = `[${method.toUpperCase()}] ${url}`;
 
   console.time(logLabel);
@@ -22,34 +24,34 @@ function logRequests(request, response, next){
   next();
 
   console.timeEnd(logLabel);
-};
+}
 
 // ------------------------------------------------------------
 
-function validateScrapId(request, response, next){
+function validateScrapId(request, response, next) {
+  const { id } = request.params;
 
-  const {id} = request.params;
-
-  if(!isUuid(id)){
-    return response.status(400).json({error: `Param sent is not a valid UUID`});
-  };
+  if (!isUuid(id)) {
+    return response
+      .status(400)
+      .json({ error: `Param sent is not a valid UUID` });
+  }
   next();
-};
+}
 
 // ------------------------------------------------------------
 
-function checkEmptyFields(request, response, next){
-
-  const {title, message} = request.body;
+function checkEmptyFields(request, response, next) {
+  const { title, message } = request.body;
 
   let checkTitle = title == null ? false : title == "" ? false : true;
   let checkMessage = message == null ? false : message == "" ? false : true;
 
-  if(checkTitle == false || checkMessage == false){
-    return response.status(400).json({error: `Blank fields!`});
-  };
+  if (checkTitle == false || checkMessage == false) {
+    return response.status(400).json({ error: `Blank fields!` });
+  }
   next();
-};
+}
 
 // ------------------------------------------------------------
 
@@ -61,16 +63,17 @@ app.use(logRequests);
 app.get("/scrapbook", (request, response) => {
   const { title } = request.query;
 
-  const card = title ? scraps.filter((scrap) => scrap.title.includes(title)) : scraps;
+  const card = title
+    ? scraps.filter((scrap) => scrap.title.includes(title))
+    : scraps;
 
   return response.json(card);
 });
 
 //acrescentar um novo card
 app.post("/scrapbook", checkEmptyFields, (request, response) => {
-
-  const {title, message} = request.body;
-  const card = {id: uuid(), title, message};
+  const { title, message } = request.body;
+  const card = { id: uuid(), title, message };
 
   scraps.push(card);
 
@@ -78,38 +81,45 @@ app.post("/scrapbook", checkEmptyFields, (request, response) => {
 });
 
 //modificar card de acordo com o id
-app.put("/scrapbook/:id", checkEmptyFields, validateScrapId, (request, response) => {
-  
-  const { id } = request.params;
-  const {title, message} = request.body;
-  const scrapIndex = scraps.findIndex((scrap) => scrap.id == id);
+app.put(
+  "/scrapbook/:id",
+  checkEmptyFields,
+  validateScrapId,
+  (request, response) => {
+    const { id } = request.params;
+    const { title, message } = request.body;
+    const scrapIndex = scraps.findIndex((scrap) => scrap.id == id);
 
-  if(scrapIndex < 0){
-    return response.status(400).json({error:`Scrap nao encontrado! 
-    ==> Id recusado: ${id} <==`})
-  };
+    if (scrapIndex < 0) {
+      return response.status(400).json({
+        error: `Scrap nao encontrado! 
+    ==> Id recusado: ${id} <==`,
+      });
+    }
 
-  const card = {
-    id,
-    title,
-    message,
-  };
+    const card = {
+      id,
+      title,
+      message,
+    };
 
-  scraps[scrapIndex] = card;
+    scraps[scrapIndex] = card;
 
-  return response.json(card);
-});
+    return response.json(card);
+  }
+);
 
 //deletar card de acordo com o id
 app.delete("/scrapbook/:id", validateScrapId, (request, response) => {
-
   const { id } = request.params;
-  const scrapIndex = scraps.findIndex((scrap) => scrap.id == id)
+  const scrapIndex = scraps.findIndex((scrap) => scrap.id == id);
 
-  if(scrapIndex < 0){
-    return response.status(400).json({"delete error":`Scrap nao encontrado! 
-    ==> Id recusado: ${id} <==`});
-  };
+  if (scrapIndex < 0) {
+    return response.status(400).json({
+      "delete error": `Scrap nao encontrado! 
+    ==> Id recusado: ${id} <==`,
+    });
+  }
 
   scraps.splice(scrapIndex, 1);
 
@@ -119,7 +129,7 @@ app.delete("/scrapbook/:id", validateScrapId, (request, response) => {
 // ------------------------------------------------------------
 
 //a partir daqui deve ser o final do arquivo
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
   console.log(`🚀 Back-end started on PORT ${port} 🚀`);
